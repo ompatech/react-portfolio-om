97746e956a8be591a5ee13a732dce75f2011bb5e
Om Patel <ompatel.patel@mail.utoronto.ca>
2024-08-16 22:40:11 -0700

    Added Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,9 @@ import Navbar from "./components/Navbar";
 import Hero from './components/Hero';
 import About from './components/About';
 import Technologies from './components/Technologies';
-import Experience from './components/Experience';
 import Projects from './components/Projects';
-import Contact from './components/Contact';
 import ContactForm from './components/ContactForm';
+import { Analytics } from "@vercel/analytics/react";
 
 const App = () => {
   return (
@@ -22,6 +21,7 @@ const App = () => {
         <Technologies />
         <Projects />
         <ContactForm />
+        <Analytics />
       </div>
     </div>
   );
